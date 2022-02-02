a9d4729698ba11447ffce872fb5fe88493142088
brunEdo <edobruno.m@gmail.com>
2022-02-02 15:51:19 +0100

    random accordions

--- src/js/async/accordions.js
+++ src/js/async/accordions.js
@@ -1,20 +1,28 @@
 import '@scssFolder/async/accordions.scss'
 
+const firstAccordion = document.querySelector('[data-open]');
+const randomElements = (el) => {
+  const rand = Math.ceil(Math.random() * 10);
+  for (let i = 0; i < rand; i++) {
+    let clone = el.cloneNode(true);
+    document.getElementById("container").append(clone);
+  }
+};
+randomElements(firstAccordion);
+
 const accordions = document.querySelectorAll('[data-open]');
 const openBtns = document.querySelectorAll('[data-btn]');
 
-console.log(accordions);
-console.log(openBtns);
-
-const openAccordion = 
-openBtns.forEach((btn, n) => {
-  btn.addEventListener("click", () => {
-    accordions.forEach((el, i) => {
-      if (i === n) {
-        el.dataset.open = !(el.dataset.open === "true");
-      } else {
-        el.dataset.open = false;
-      }
-    });
+const openAccordion = (n) => {
+  accordions.forEach((el, i) => {
+    if (i === n) {
+      el.dataset.open = !(el.dataset.open === "true");
+    } else {
+      el.dataset.open = false;
+    }
   });
+}
+
+openBtns.forEach((btn, i) => {
+  btn.addEventListener("click", () => openAccordion(i));
 })
